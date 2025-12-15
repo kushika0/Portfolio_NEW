65187a4ede11a84a43c64e8874dafe55acfb7084
kushika0 <kushika1404@gmail.com>
2025-12-15 21:30:26 +0530

    projects

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import About from './components/About'
+import Project from './components/Project'
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Home/>
       <About/>
+      <Project/>
     </div>
   )
 }
